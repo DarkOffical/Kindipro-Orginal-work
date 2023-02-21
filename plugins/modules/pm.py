3a348fa107815ed402443a283d6d0b4d7d8a80d6
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-02-22 03:43:37 +0530

    Update pm.py

--- plugins/modules/pm.py
+++ plugins/modules/pm.py
@@ -11,7 +11,7 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-@Client.on_message(filters.command("") & filters.text)
+@Client.on_message(filters.command("r") & filters.text)
 async def pm_text(client: Client, message):
     try:
         if message.from_user.id == ADMIN:
